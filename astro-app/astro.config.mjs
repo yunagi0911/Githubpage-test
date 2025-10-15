e7a7eec51290dd2c3595964a677bc8834b7a30c2
yunagi <ynagi0911@gmail.com>
2025-10-16 00:14:10 +0900

    初回コミット

---
+++ astro-app/astro.config.mjs
@@ -0,0 +1,17 @@
+// @ts-check
+
+import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+// https://astro.build/config
+export default defineConfig({
+  vite: {
+      plugins: [tailwindcss()],
+	},
+
+  integrations: [react()],
+   site: 'https://yunagi0911.github.io',
+    base: '/Githubpage-test',
+});
