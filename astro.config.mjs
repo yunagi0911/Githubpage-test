ac7c6165ddb324b2aa814fbe0d6fe09a4b490524
yunagi <ynagi0911@gmail.com>
2025-10-24 18:06:09 +0900

    ssr確認

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,13 +5,16 @@ import { defineConfig } from 'astro/config';
 
 import react from '@astrojs/react';
 
+import netlify from '@astrojs/netlify';
+
 // https://astro.build/config
 export default defineConfig({
   vite: {
       plugins: [tailwindcss()],
-	},
+    },
 
   integrations: [react()],
   site: 'https://yunagi0911.github.io',
   base: '/Githubpage-test',
+  adapter: netlify(),
 });
